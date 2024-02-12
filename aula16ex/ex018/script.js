2503ed391e9f1045ae9c16b96edb3b24fbb8d544
Leandro Sousa <156814218+LSH2k@users.noreply.github.com>
2024-02-12 09:27:31 -0300

    Update script.js

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -25,6 +25,7 @@ function adicionar() {
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado.`
         lista.appendChild(item)
+        // res.innerHTML = ''
     } else {
         alert('Valor inválido ou já encontrado na lista.')
     }
@@ -37,8 +38,23 @@ function finalizar() {
         alert('Adicione valores antes de finalizar!')
     } else {
         let tot = valores.length
-
-        res.innerHTML = ''
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / tot
+        // res.innerHTML = ''
         res.innerHTML += `<p>Ao todo temos <strong>${tot}</strong> números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi <strong>${maior}</strong>.</p>`
+        res.innerHTML += `<p>O menor valor informado foi <strong>${menor}</strong>.</p>`
+        res.innerHTML += `<p>A soma de todos os valores foi <strong>${soma}</strong>.</p>`
+        res.innerHTML += `<p>A média de todos os valores foi <strong>${media}</strong>.</p>`
     }
 }
